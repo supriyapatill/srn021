af39ca69253700e713f02b6c074cf274d6061c59
supriyapatill <supriyapatil0666@gmail.com>
2025-11-15 08:53:01 +0530

    Add basic math operations with error handling
    Fix indentation and syntax errors in math.py.

---
+++ math.py
@@ -0,0 +1,9 @@
+try:
+  a=float(input("enter first number:"))
+  b=float(input("enter second number:"))
+  
+  print("f addition:{a+b}")
+    print("f substraction:{a-b}")
+
+expect ValueError:
+print("please enter valid numbers:")
